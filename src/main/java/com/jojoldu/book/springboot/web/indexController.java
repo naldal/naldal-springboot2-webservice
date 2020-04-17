9b108dfefccc3b873785931645aba3d7fa472dc8
naldal <cec7378@gmail.com>
2020-04-17 16:34:45 +0900

    apply spring security

--- src/main/java/com/jojoldu/book/springboot/web/indexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/indexController.java
@@ -1,5 +1,7 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.LoginUser;
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +11,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class indexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성함
+        if(user != null) {
+        //세션에 저장된 값이 있을 때만 model 에 userName으로 등록함. 세션에 저장된 값이 없으면 model에는 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
